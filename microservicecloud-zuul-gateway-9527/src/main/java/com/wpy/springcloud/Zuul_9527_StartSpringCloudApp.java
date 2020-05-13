2d2c26a2026e7a673a07340cb801c7ca01f785cc
Wang Pengyue <wangpengyue@liuyuandeMacBook-Pro.local>
2020-05-13 18:01:16 +0800

    add zuul proxy

---
+++ microservicecloud-zuul-gateway-9527/src/main/java/com/wpy/springcloud/Zuul_9527_StartSpringCloudApp.java
@@ -0,0 +1,16 @@
+package com.wpy.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient // 本服务启动后自动注册进eureka服务中
+public class Zuul_9527_StartSpringCloudApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul_9527_StartSpringCloudApp.class, args);
+    }
+}
